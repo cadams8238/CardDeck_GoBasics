9e940f62daef4666eaa2bc3dead15c34126641ef
Courtney Adams <cadams8238@gmail.com>
2018-11-22 15:16:40 -0800

    added newCard() function. basic implementation, simply returns a hard coded string

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	// var card = "Ace of Spades"
-	card := "Ace of Spades"
+	card := newCard()
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
