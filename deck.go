b1aab329652c6a6f472c6eab0fbc61a7d0786f31
Courtney Adams <cadams8238@gmail.com>
2018-11-24 13:13:24 -0800

    added newDeck() function that creates a new deck of cards when called

--- deck.go
+++ deck.go
@@ -4,9 +4,28 @@ import (
 	"fmt"
 )
 
-// Create a new type of 'deck'
-// which is a slice of strings
+// Create a new type of 'deck' which is a slice of
+// strings
 type deck []string
+
+// not a receiver function because we won't have a
+// deck yet to work with, since receiver funcs only
+// work if you have an instance of that type created
+// to do something with
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits { // _ means we know there's a variable there but we don't need to use it
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
 
 func (d deck) print() {
 	for i, card := range d {
--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Five of Spades")
+	cards := newDeck()
 
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
