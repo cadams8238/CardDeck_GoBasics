9ee1cf32bd18571844c61311668ca3dab2f6e074
Courtney Adams <cadams8238@gmail.com>
2018-11-24 14:31:32 -0800

    added saveToFile() receiver function that takes a filename as an argument and converts a deck of cards to a single string which then gets turned into a slice of bytes and saves to local machine

--- deck.go
+++ deck.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
 )
 
 // Create a new type of 'deck' which is a slice of
@@ -36,4 +38,14 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+// receiver function
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+// receiver function
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
